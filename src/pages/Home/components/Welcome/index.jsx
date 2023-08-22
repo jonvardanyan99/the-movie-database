978b8f47055fa9834f66fbc527330d4d4eec3651
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-08-22 12:08:42 +0400

    add searching functionality

--- src/pages/Home/components/Welcome/index.jsx
+++ src/pages/Home/components/Welcome/index.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 import styles from './styles.module.scss';
 
 export const Welcome = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = event => {
+        setInputValue(event.target.value);
+    };
+
     return (
         <div className={styles.welcome}>
             <h2>Welcome.</h2>
             <h3>Millions of movies, TV shows and people to discover. Explore now.</h3>
             <div>
-                <input placeholder="Search for a movie, tv show, person......" />
-                <button>Search</button>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Search for a movie, tv show, person......"
+                />
+                <Link to={`/search/movie/${inputValue}`}>Search</Link>
             </div>
         </div>
-    )
-}
+    );
+};
