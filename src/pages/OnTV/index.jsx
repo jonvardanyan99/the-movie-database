19e1cbe4be9211fee687acc09910280203d3570a
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-08-22 11:50:35 +0400

    create currently airing tv shows page

---
+++ src/pages/OnTV/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import { FilmsVerticalList } from '../../components/FilmsVerticalList';
+import { useQuery } from '../../hooks/useQuery';
+
+import styles from './styles.module.scss';
+
+export const OnTV = () => {
+    const [pageNum, setPageNum] = useState(1);
+    const {loading, data = {}} = useQuery({ url: '/tv/on_the_air', params: `&language=en-US&page=${pageNum}` });
+
+    const handlePageChange = () => {
+        setPageNum(pageNum + 1);
+    };
+
+    return (
+        <div className={styles['on-the-air']}>
+            <FilmsVerticalList
+                title='Currently Airing TV Shows'
+                loading={loading}
+                data={data}
+                handlePageChange={handlePageChange}
+                mediaType='tv'
+            />
+        </div>
+    );
+};
