2690615d78f221f30b3727e8a747bb7a76199ca8
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-08-22 11:59:01 +0400

    create top rated tv shows page

---
+++ src/pages/TopRatedTVShows/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+import { FilmsVerticalList } from '../../components/FilmsVerticalList';
+import { useQuery } from '../../hooks/useQuery';
+
+import styles from './styles.module.scss';
+
+export const TopRatedTVShows = () => {
+    const [pageNum, setPageNum] = useState(1);
+    const {loading, data = {}} = useQuery({ url: '/tv/top_rated', params: `&language=en-US&page=${pageNum}` });
+
+    const handlePageChange = () => {
+        setPageNum(pageNum + 1);
+    };
+
+    return (
+        <div className={styles['top-rated']}>
+            <FilmsVerticalList
+                title='Top Rated TV Shows'
+                loading={loading}
+                data={data}
+                handlePageChange={handlePageChange}
+                mediaType='tv'
+            />
+        </div>
+    );
+};
