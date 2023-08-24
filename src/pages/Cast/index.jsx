3a94cb386a2d45658c769b78f71a21a4f21cd428
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-08-24 12:44:40 +0400

    add data fetching logic

--- src/pages/Cast/index.jsx
+++ src/pages/Cast/index.jsx
@@ -1,11 +1,26 @@
+import { useParams } from 'react-router-dom';
+
+import { useQuery } from '../../hooks/useQuery';
+
 import { Overview } from './components/Overview';
 import { PeopleInfo } from './components/PeopleInfo';
 
 export const Cast = () => {
+    const params = useParams();
+
+    const {loading, data} = useQuery({ url: `/${params.mediaType}/${params.id}`, params: '&language=en-US&page=1' });
+
     return (
         <div>
-            <Overview />
-            <PeopleInfo />
+            <Overview
+                loading={loading}
+                name={data?.title || data?.name || ''}
+                date={data?.release_date || data?.first_air_date || ''}
+                posterPath={data?.poster_path || ''}
+            />
+            <PeopleInfo
+                name={data?.title || data?.name || ''}
+            />
         </div>
     );
 };
