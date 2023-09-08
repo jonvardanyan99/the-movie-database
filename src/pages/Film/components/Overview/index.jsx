05c71502919408680c8c8757b771feef4a3af945
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-09-08 12:25:41 +0400

    change the codes for the action icons

--- src/pages/Film/components/Overview/index.jsx
+++ src/pages/Film/components/Overview/index.jsx
@@ -112,17 +112,23 @@ export const Overview = ({ loading, id, name, date, genres, runtime, posterPath,
                                 <Percentage percent={voteAverage * 10 || 0} className={styles['overview-percentage']} />
                                 <span>User Score</span>
                             </div>
-                            <div className={styles.action}>
-                                <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-159-thumbnails-list-white-c260ea972eebf812289fd3c41d0d2c1dff33ecbcd62be13fba8eeaf9de173789.svg" alt="lists" />
-                            </div>
-                            <div className={styles.action}>
-                                <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-13-heart-white-28d2cc2d6418c5047efcfd2438bfc5d109192671263c270993c05f130cc4584e.svg" alt="favourite" />
-                            </div>
-                            <div className={styles.action}>
-                                <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-73-bookmark-white-432e98d550b7e4c80b06272c49475b0db85a60f6fae450420713004b3c9d3ffd.svg" alt="watchlist" />
-                            </div>
-                            <div className={styles.action}>
-                                <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-49-star-white-5c85220678b312aea9599d5f12ad858a9e7df226de51ef8b6b699023ffeda5fa.svg" alt="rate" />
+                            <div className={styles['actions-container']}>
+                                <div>
+                                    <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-159-thumbnails-list-white-c260ea972eebf812289fd3c41d0d2c1dff33ecbcd62be13fba8eeaf9de173789.svg" alt="lists" />
+                                    <span>Login to create and edit custom lists</span>
+                                </div>
+                                <div>
+                                    <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-13-heart-white-28d2cc2d6418c5047efcfd2438bfc5d109192671263c270993c05f130cc4584e.svg" alt="favourite" />
+                                    <span>Login to add this film to your favorite list</span>
+                                </div>
+                                <div>
+                                    <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-73-bookmark-white-432e98d550b7e4c80b06272c49475b0db85a60f6fae450420713004b3c9d3ffd.svg" alt="watchlist" />
+                                    <span>Login to add this film to your watchlist</span>
+                                </div>
+                                <div>
+                                    <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-49-star-white-5c85220678b312aea9599d5f12ad858a9e7df226de51ef8b6b699023ffeda5fa.svg" alt="rate" />
+                                    <span>Login to rate this film</span>
+                                </div>
                             </div>
                             <div>
                                 <img src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-175-play-806cb05551791b8dedd7f8d38fd3bd806e2d397fcfeaa00a5cc9129f0819fd07.svg" alt="play" />
