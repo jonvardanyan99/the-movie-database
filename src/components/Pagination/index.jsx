3651b9d3bc242716f349d9750017c8d4fc236321
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-10-06 10:58:54 +0400

    make some changes for the component

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -2,55 +2,61 @@ import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
-export const Pagination = ({ currPage, onPageChange, totalPages }) => {
-    let arr = [];
+export const Pagination = ({ currentPage, onPageChange, totalPages }) => {
+    let pageNumbers = [];
 
     for (let i = 1; i <= totalPages; i++) {
-        arr.push(i);
+        pageNumbers.push(i);
     };
 
-    let endNumbers = [arr.length - 1, arr.length];
-    let length = arr.length;
+    let endNumbers = [pageNumbers.length - 1, pageNumbers.length];
+    let length = pageNumbers.length;
 
-    if (arr.length >= 12) {
-        if (currPage <= 4) {
-            arr = arr.slice(0, 7).concat(['...', ...endNumbers]);
-        } else if (currPage >= 5 && currPage <= 7) {
-            arr = arr.slice(0, currPage + 3).concat(['...', ...endNumbers]);
-        } else if (currPage >= 8 && currPage <= length - 7) {
-            arr = arr.slice(0, 2).concat('...').concat(arr.slice(currPage - 4, currPage + 3)).concat(['...', ...endNumbers]);
-        } else if (currPage >= length - 6 && currPage <= length - 4) {
-            arr = arr.slice(0, 2).concat('...').concat(arr.slice(currPage - 4));
-        } else if (currPage >= length - 3) {
-            arr = arr.slice(0, 2).concat('...').concat(arr.slice(length - 7));
+    if (pageNumbers.length >= 12) {
+        if (currentPage <= 4) {
+            pageNumbers = pageNumbers.slice(0, 7).concat(['...', ...endNumbers]);
+        } else if (currentPage >= 5 && currentPage <= 7) {
+            pageNumbers = pageNumbers.slice(0, currentPage + 3).concat(['...', ...endNumbers]);
+        } else if (currentPage >= 8 && currentPage <= length - 7) {
+            pageNumbers = pageNumbers.slice(0, 2).concat('...').concat(pageNumbers.slice(currentPage - 4, currentPage + 3)).concat(['...', ...endNumbers]);
+        } else if (currentPage >= length - 6 && currentPage <= length - 4) {
+            pageNumbers = pageNumbers.slice(0, 2).concat('...').concat(pageNumbers.slice(currentPage - 4));
+        } else if (currentPage >= length - 3) {
+            pageNumbers = pageNumbers.slice(0, 2).concat('...').concat(pageNumbers.slice(length - 7));
         };
     };
-
+    
     return (
         totalPages > 1 ? (
-            <ul className={styles.pagination}>
-                {currPage > 1 && <button onClick={() => onPageChange(currPage - 1)}>← Previous</button>}
-                {arr.map((item, index) => (
-                    <li
-                        key={index}
-                        onClick={() => {
-                            if (typeof(item) === 'number') {
-                                onPageChange(item);
-                            };
-                        }}
-                        className={item === currPage ? styles['curr-page'] : item === '...' ? styles.ellipsis : undefined}
-                    >
-                        {item}
-                    </li>
-                ))}
-                {currPage < length && <button onClick={() => onPageChange(currPage + 1)}>Next →</button>}
-            </ul>
+            <div className={styles.pagination}>
+                {currentPage > 1 &&
+                <button onClick={() => onPageChange(currentPage - 1)}>
+                    ← Previous
+                </button>}
+                {pageNumbers.map((item, index) => {
+                    const isNumber = typeof item === 'number';
+
+                    return (
+                        <button
+                            key={index}
+                            onClick={isNumber ? () => onPageChange(item) : undefined}
+                            className={item === currentPage ? styles['curr-page'] : isNumber ? undefined : styles.ellipsis}
+                        >
+                            {item}
+                        </button>
+                    );
+                })}
+                {currentPage < length &&
+                <button onClick={() => onPageChange(currentPage + 1)}>
+                    Next →
+                </button>}
+            </div>
         ) : null
     );
 };
 
 Pagination.propTypes = {
-    currPage: PropTypes.number.isRequired,
+    currentPage: PropTypes.number.isRequired,
     onPageChange: PropTypes.func.isRequired,
     totalPages: PropTypes.number.isRequired,
 };
