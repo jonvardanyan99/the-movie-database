c03ddfd4266ff516f021b39fbe7ae81b39f2d7d1
jonvardanyan99 <john.vardanyan99@gmail.com>
2023-11-27 12:03:01 +0400

    change spaces and end of line sequence

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -3,21 +3,21 @@ import { useState, useEffect } from 'react';
 import { TMDB_API_KEY } from '../configs';
 
 export const useQuery = ({ url, params, skip }) => {
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState();
 
-    useEffect(() => {
-        if (!skip) {
-            setLoading(true);
+  useEffect(() => {
+    if (!skip) {
+      setLoading(true);
 
-            fetch(`https://api.themoviedb.org/3${url}?api_key=${TMDB_API_KEY}${params}`)
-                .then(response => response.json())
-                .then(data => {
-                    setData(data);
-                    setLoading(false);
-                });
-        };
-    }, [url, params, skip]);
+      fetch(`https://api.themoviedb.org/3${url}?api_key=${TMDB_API_KEY}${params}`)
+        .then(response => response.json())
+        .then(data => {
+          setData(data);
+          setLoading(false);
+      });
+    };
+  }, [url, params, skip]);
 
-    return {loading, data};
+  return {loading, data};
 };
